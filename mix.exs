7cd17fe428ab8b1c879c7a961c3de3f48cfc2654
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-05 20:38:42 +0530

    absinthe 1.3.2 absinthe_ecto 0.1.3 absinthe_plug 1.3.0

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule Community.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe_ecto, "~> 0.1.0"},
+      {:absinthe_plug, "~> 1.3.0"}
     ]
   end
 
