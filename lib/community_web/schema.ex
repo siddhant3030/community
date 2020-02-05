a2ba22cb642680aea441759da9f2c57754a963d6
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-05 20:48:36 +0530

    error caught

---
+++ lib/community_web/schema.ex
@@ -0,0 +1,15 @@
+defmodule CommunityWeb.Schema do
+  use Absinthe.Schema
+
+  alias CommunityWeb.NewsResolver
+
+  object :link do
+    field :id, non_null(:id)
+    field :url, non_null(:string)
+    field :description, non_null(:string)
+  end
+
+  query do
+    field :all_links, non_null(list_of(non_null(:link)))
+  end
+end
